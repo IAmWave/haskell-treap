4bdc8c8e96b88ca5634f2ab6f7b14a6e0851e9de
Václav <vaclav.volhejn@gmail.com>
2018-05-22 13:52:54 +0200

    Add tree visualisation

--- treap.hs
+++ treap.hs
@@ -1,4 +1,5 @@
 import System.Random
+import qualified Data.List as List
 
 type Weight = Int
 
@@ -7,12 +8,25 @@ data Treap v = Empty | Treap Weight v (Treap v) (Treap v)
     deriving (Eq)
 
 instance Show a => Show (Treap a) where
-    show Empty = ""
-    show (Treap w a ls rs) = (show a)++"|"++(show w)++"(" ++ (show ls) ++ ";" ++ (show rs) ++ ")"
+    show = showTree
+    -- show Empty = ""
+    -- show (Treap w a ls rs) = (show a)++"|"++(show w)++"(" ++ (show ls) ++ ";" ++ (show rs) ++ ")"
 
 
 ex = Treap 9 'h' Empty (Treap 7 'j' Empty Empty)
 eg = mkStdGen 123
+
+empty :: Treap a
+empty = Empty
+
+singleton :: RandomGen g => g -> a -> (g, Treap a)
+singleton g a = (g', Treap w a Empty Empty)
+    where (w, g') = next g
+
+-------------- Lookup --------------
+isEmpty :: Treap a -> Bool
+isEmpty Empty = True
+isEmpty t = False
 
 member :: Ord a => a -> Treap a -> Bool
 member _ Empty = False
@@ -22,19 +36,10 @@ member a (Treap _ v ls rs)
     | a > v     = member a rs
 
 
-empty :: Treap a
-empty = Empty
-
-
-singleton :: RandomGen g => g -> a -> (g, Treap a)
-singleton g a = (g', Treap w a Empty Empty)
-    where (w, g') = next g
-
-
+-------------- Modification --------------
 insert :: (RandomGen g, Ord a) => g -> a -> Treap a -> (g, Treap a)
 insert g a treap = (g', insertWithWeight w a treap)
     where (w, g') = next g
-
 
 -- First insert at the bottom, then rotate to maintain the heap invariant
 insertWithWeight :: Ord a => Weight -> a -> Treap a -> Treap a
@@ -54,7 +59,6 @@ rotateL (Treap w a (Treap lw la lls lrs) rs)
     | lw > w      = Treap lw la lls (Treap w a lrs rs)
     | otherwise   = (Treap w a (Treap lw la lls lrs) rs)
 
-
 -- Maintain the heap invariant of the right son
 rotateR :: Ord a => Treap a -> Treap a
 rotateR Empty = Empty
@@ -63,12 +67,11 @@ rotateR (Treap w a ls (Treap rw ra rls rrs))
     | rw > w      = Treap rw ra (Treap w a ls rls) rrs
     | otherwise   = Treap w a ls (Treap rw ra rls rrs)
 
-
+-------------- List conversion --------------
 fromList :: (RandomGen g, Ord a) => g -> [a] -> (g, Treap a)
 fromList g [] = (g, Empty)
 fromList g (a:as) = insert g' a rest
     where (g', rest) = fromList g as
-
 
 toList :: Treap a -> [a]
 toList l = toList' l []
@@ -79,5 +82,23 @@ toList l = toList' l []
                   acc1 = toList' rs acc0
 
 
+-------------- Debugging --------------
+showTree :: Show a => Treap a -> String
+showTree t = showTree' t []
+
+indent :: [Bool] -> String
+indent [] = ""
+indent (x:xs) = (foldl (++) "" $ reverse $ map (\x -> if x then "|  " else "   ") xs) ++ "+--"
+
+showTree' :: Show a => Treap a -> [Bool] -> String
+showTree' Empty d = (indent d) ++ "ø\n"
+showTree' (Treap w a ls rs) d = myLine ++ "\n" ++ rest
+    where
+        myLine = (indent d) ++ (show a) ++ " (" ++ (show w) ++ ")"
+        rest = if ((isEmpty ls) && (isEmpty rs))
+            then ""
+            else ((showTree' ls (True:d)) ++ (showTree' rs (False:d)))
+
 (g1, t1) = insert eg 'd' empty
 (g2, t2) = insert g1 'a' t1
+t = snd $ fromList eg [1..10]
